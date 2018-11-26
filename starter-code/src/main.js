fd0e060605293930d67d6816e5a74216a68d8a05
mrosas22 <miller.rosas22@gmail.com>
2018-11-26 12:59:37 -0500

    Added binding to DOM

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,19 +27,51 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  memoryGame.shuffleCards(cards);
   var html = '';
-  memoryGame.cards.forEach(function (pic) {
-    html += '<div class="card" data-card-name="'+ pic.name +'">';
-    html += '  <div class="back" name="'+ pic.img +'"></div>';
-    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
-    html += '</div>';
+  memoryGame.cards.forEach(function (pic, index) {
+  html += '<div class= "card" id="card_' + pic.name + '">';
+  html += '<div class="back"';
+  html += ' name="' + pic.img + '">';
+  html += '</div>';
+  html += '<div class="front" ';
+  html += 'style="background: url(img/' + pic.img + ') no-repeat">';
+  html += '</div>';
+  html += '</div>';
   });
-
   // Add all the div's to the HTML
-  $('#memory_board').html(html);
-
+  document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
-  $('.back').click(function () {
-    // TODO: write some code here
+  
+  
+  $('.back').on('click', function(){
+    // temporarily hide the back class
+    $(this).hide();
+    // add the 'back' class to the front div to make visible
+    $(this).parent().find('.front').addClass('back');
+  // hold the first card picked as a reference for the next card to compare
+  if (memoryGame.pickedCards.length === 0) {
+  memoryGame.pickedCards.push($(this).attr('name'))
+  } else {
+  // if one card has already been picked, compare
+  if (memoryGame.checkIfPair(memoryGame.pickedCards[0], $(this).attr('name'))){
+  // reset array, to restart process
+  memoryGame.pickedCards = [];
+  memoryGame.finished();
+  } else {
+  // repeat the reverse of what happened before!
+  // The set timeout will leave cards visible a moment
+  setTimeout(() => {
+  // to reput the back div of the 2nd clicked card
+  $(this).show()
+  $(this).parent().find('.front').removeClass('back');
+  // to reput the back div of the 1st clicked card (stored in the array)
+  $(`[name="${memoryGame.pickedCards[0]}"]`).show();
+  $(`[name="${memoryGame.pickedCards[0]}"]`).parent().find('.front').removeClass('back');
+  // reset pickedCards array to begin again
+  memoryGame.pickedCards = [];
+  }, 1000)
+  };
+  };
   });
-});
+  })
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -4,47 +4,59 @@ var MemoryGame = function (cards) {
   this.pickedCards = [];
   this.pairsClicked = 0;
   this.pairsGuessed = 0;
+  // this.cards = this.shuffleCards(cards);
 };
 
 
-MemoryGame.prototype.shuffleCards = function () {
-  //Should mixed the cards property
-  let shuffle = [], 
-  j = cards.length;
-  //While there remain elements to shuffle…
-  for (i=0; i< cards.length;i++ ) { 
-  //Pick a remaining element…
-  x = Math.floor(Math.random() *(cards.length )); 
-  //And move it to the new array.
-  shuffle.push(cards.splice(x, 1));
+MemoryGame.prototype.shuffleCards = function (array) {
+  return;
+  var m = array.length, t, i;
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
   }
-  return shuffle;
-  };
+
+  return array;
+}
 
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
   // It should add 1 to `pairsClicked` when we call it
+  this.pairsClicked++;
+  $('#pairs_clicked').html(this.pairsClicked);
   if(firstCard=== secondCard){
     //It should add 1 to pairsGuessed if they are the same card
     this.pairsGuessed++;
-    this.pairsClicked++;
     $('#pairs_guessed').html(this.pairsGuessed);
-    $('#pairs_clicked').html(this.pairsClicked);
     //It should return true when the comparing cards are the same
     return true;
   } else {
-    this.pairsClicked++;
-    $('#pairs_clicked').html(this.pairsClicked);
     //It should return false when the comparing cards are the same
     return false;
   }
-  
+  // clear the array since it always has to have max two numbers
+  this.pickedCards = [];
+  // check if we have 12 correct guesses
+  this.finished();
 }
 
 
 MemoryGame.prototype.finished = function () {
-  if (this.pairsGuessed === (cards.length)/2){
+  if (this.pairsGuessed === 12){
     return true;
-  };
+      // clear the board
+      $(".card").toggle();
+
+      // append h1 with text "YOU WON!!!!"
+      $("#memory_board").append("<h1>YOU WON!!!!</h1>")
+  }else{
   return false;
+  }
 };
